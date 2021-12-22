87ac3e8645510be0681997669e87e3e43ac70cbf
Sandro Coutinho <sandro@farrelltech.org>
2021-12-22 14:00:53 +0000

    [TASK] Added render.yaml

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: "0.0.0.0",
+  url: env("RENDER_EXTERNAL_URL"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'XCkvU0R8o9BWYDQrulPJG5K4AyJUMn3I'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
